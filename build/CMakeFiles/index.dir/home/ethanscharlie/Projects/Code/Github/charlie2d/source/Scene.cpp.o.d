84b5d8df5abdd540f7559b6aba10a38ea29bb14c
Ethanscharlie <ethanscharlie@protonmail.com>
2023-12-09 03:43:02 -0700

    Removed builds

--- build/CMakeFiles/index.dir/home/ethanscharlie/Projects/Code/Github/charlie2d/source/Scene.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/index.dir/home/ethanscharlie/Projects/Code/Github/charlie2d/source/Scene.cpp.o: \
- /home/ethanscharlie/Projects/Code/Github/charlie2d/source/Scene.cpp \
- /usr/include/stdc-predef.h
